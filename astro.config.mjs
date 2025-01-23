c5bec54cadd106997c6bc2b700467e50e8055ea3
James McEwan <60427+jamesmcewan@users.noreply.github.com>
2025-01-23 22:26:39 +0000

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-import vercel from '@astrojs/vercel'
-
-import netlify from '@astrojs/netlify';
+import netlify from '@astrojs/netlify'
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,3 +8,4 @@ export default defineConfig({
   output: 'server',
   adapter: netlify(),
 })
+
