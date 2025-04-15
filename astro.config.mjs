1423388b6ea888d9544c558087fa614fced33ebe
James McEwan <60427+jamesmcewan@users.noreply.github.com>
2025-04-15 22:19:23 +0100

    cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config'
-import vercel from '@astrojs/vercel'
 import tailwindcss from '@tailwindcss/vite'
+import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: cloudflare(),
   vite: {
     plugins: [tailwindcss()],
   },
