f7beb7d4e4228e5d69f824a63dad988ae58f7bb2
James McEwan <60427+jamesmcewan@users.noreply.github.com>
2025-04-16 16:34:58 +0100

    flyyyyy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
-import cloudflare from '@astrojs/cloudflare'
+
+import node from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare(),
+  adapter: node({
+    mode: 'standalone',
+  }),
   vite: {
     plugins: [tailwindcss()],
   },
