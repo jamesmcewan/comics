53fc474ee13b496fe64a2c13f67bc6d3a34001bf
James McEwan <60427+jamesmcewan@users.noreply.github.com>
2025-04-26 17:46:47 +0100

    add tests

--- tests/setup.js
+++ tests/setup.js
@@ -1,11 +1,4 @@
 import { expect, afterEach } from 'vitest'
-// import { cleanup } from '@testing-library/dom'
 import '@testing-library/jest-dom'
 
-// // Clean up after each test
-// afterEach(() => {
-//   cleanup()
-// })
-
-// Extend expect with jest-dom matchers
 Object.defineProperty(globalThis, 'expect', { value: expect })
